c2ef61feb1bb9ff134e086f723c913c777d09885
Jason Waseq <jwaseq@ucsc.edu>
2024-11-07 17:39:43 +0000

    Adding finished three.c

---
+++ midterm/three.c
@@ -0,0 +1,16 @@
+#include "midterm.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t count_strings_with_property(bool (*property)(char *), char **strings,
+                                   size_t len) {
+    size_t count = 0;
+    for (size_t i = 0; i < len; i++) {
+        if (property(strings[i])) {
+            count++;
+        }
+    }
+    return count;
+}
